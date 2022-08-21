8fad975217e72e0d5a496390d4625aaa866fca6d
Naman Jain <103898041+Namanj413@users.noreply.github.com>
2022-08-21 20:51:21 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,10 +3,12 @@ content = document.querySelector(".content"),
 selectMenu = document.querySelectorAll("select"),
 setAlarmBtn = document.querySelector("button");
 
+//Alaram tune linked...
 let alarmTime, isAlarmSet,
 ringtone = new Audio("./files/ringtone.mp3");
 
 function options() {
+    // option hour for alaram
     selectMenu[0].innerHTML = selectMenu[1].innerHTML = selectMenu[2].innerHTML = "";
     selectMenu[0].innerHTML = `<option value="Hour" selected disabled hidden>Hour</option>`;
     for (let i = 12; i > 0; i--) {
@@ -15,6 +17,8 @@ function options() {
         selectMenu[0].firstElementChild.insertAdjacentHTML("afterend", option);
     }
     
+    
+    // option minut for alaram
     selectMenu[1].innerHTML = `<option value="Minute" selected disabled hidden>Minute</option>`;
     for (let i = 59; i >= 0; i--) {
         i = i < 10 ? `0${i}` : i;
@@ -22,6 +26,7 @@ function options() {
         selectMenu[1].firstElementChild.insertAdjacentHTML("afterend", option);
     }
     
+    // option AM/PM for alaram
     selectMenu[2].innerHTML = `<option value="AM/PM" selected disabled hidden>AM/PM</option>`;
     for (let i = 2; i > 0; i--) {
         let ampm = i == 1 ? "AM" : "PM";
@@ -31,6 +36,7 @@ function options() {
 }
 options();
 
+// Input data from user
 setInterval(() => {
     let date = new Date(),
     h = date.getHours(),
@@ -41,18 +47,21 @@ setInterval(() => {
         h = h - 12;
         ampm = "PM";
     }
+    // if hours min sec 10 se kam hai to peeche '0' lagega
     h = h == 0 ? h = 12 : h;
     h = h < 10 ? "0" + h : h;
     m = m < 10 ? "0" + m : m;
     s = s < 10 ? "0" + s : s;
     currentTime.innerText = `${h}:${m}:${s} ${ampm}`;
 
+    // alamram time matched then tone will play
     if (alarmTime === `${h}:${m} ${ampm}`) {
         ringtone.play();
         ringtone.loop = true;
     }
 });
 
+// Alaram set fuction
 function setAlarm() {
     if (isAlarmSet) {
         alarmTime = "";
@@ -63,10 +72,12 @@ function setAlarm() {
         return options();
     }
 
+    // if alaram time is not selected right then error occurs
     let time = `${selectMenu[0].value}:${selectMenu[1].value} ${selectMenu[2].value}`;
     if (time.includes("Hour") || time.includes("Minute") || time.includes("AM/PM")) {
         return alert("Please, select a valid time to set Alarm!");
     }
+    // clear alaram function
     alarmTime = time;
     isAlarmSet = true;
     content.classList.add("disable");
